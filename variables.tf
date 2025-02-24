c5a95b503670f46121e39d4b458331e749c5e777
matthew graham <matthew.graham@abrigo.com>
2025-02-24 09:54:47 -0500

    Update from last module

--- variables.tf
+++ variables.tf
@@ -37,14 +37,14 @@ variable "api_key" {
   description = "(Required) API key for web app to talk to SaaS platform."
 }
 
-variable "public_subnets" {
-  type        = list(string)
-  description = "(Required) List of subnet IDs for EC2 instance deployments."
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "(Required) VPC ID of VPC for application deployment."
-}
+# variable "public_subnets" {
+#   type        = list(string)
+#   description = "(Required) List of subnet IDs for EC2 instance deployments."
+# }
+#
+# variable "vpc_id" {
+#   type        = string
+#   description = "(Required) VPC ID of VPC for application deployment."
+# }
 
 
