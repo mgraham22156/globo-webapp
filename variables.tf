dd4ffcef5f8568f29b25271ab5daffd03c6ee9e2
matthew graham <matthew.graham@abrigo.com>
2025-02-24 16:48:51 -0500

    Switch to using secrets manager

--- variables.tf
+++ variables.tf
@@ -32,23 +32,28 @@ variable "instance_type" {
   default     = "t3.micro"
 }
 
-variable "api_key" {
+variable "api_key_secret_id" {
   type        = string
-  description = "(Required) API key for web app to talk to SaaS platform."
+  description = "(Required) Secret ID for API key for web app to talk to SaaS platform."
 }
-variable "playbook_respository" {
+
+variable "playbook_repository" {
   type        = string
-  description = "(Required) URL of playbook repository."
+  description = "(Required) URL of Ansible playbook repository."
 }
 
-variable "public_subnets" {
-  type        = list(string)
-  description = "(Required) List of subnet IDs for EC2 instance deployments."
-}
-
-variable "vpc_id" {
+variable "ec2_role_name" {
   type        = string
-  description = "(Required) VPC ID of VPC for application deployment."
+  description = "(Required) Name of role to associate with EC2 instance profile."
 }
 
+# TFC variables
+variable "tfe_organization" {
+  type        = string
+  description = "(Required) Name of TFC organization."
+}
 
+variable "tfe_workspace_name" {
+  type        = string
+  description = "(Required) Name of networking workspace to get information."
+}
